a94e9d83ad39161a7d7cd02e9ee2eec488c2d9cf
Arjan Dangol <arjan.dangol95@gmail.com>
2023-06-23 08:21:40 +1000

    Handle search with enter key

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/reducers/hooks";
 import {
@@ -21,6 +21,11 @@ export default function SearchBox() {
       dispatch(getSearchedImages());
     }
   };
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13) {
+      handleSearch();
+    }
+  };
   return (
     <>
       {/* Search area start */}
@@ -38,6 +43,7 @@ export default function SearchBox() {
           id="searchTerm"
           value={searchTerm}
           onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
+          onKeyDown={(e) => handleKeyPress(e)}
           className="block w-full rounded-full border-0 px-3.5 py-2 pl-12 pr-8 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
         <div className="absolute inset-y-0 right-0 pr-2 flex items-center">
